c051b628749d63f16378ae994062832c9dda8ea5
eugenox <eugenox@gmail.com>
2014-04-12 14:34:13 +0200

    License update.

--- license.js
+++ license.js
@@ -1,4 +1,4 @@
-// Copyright (c) 2012-2013 SCRIPT_AUTHOR
+// Copyright (c) 2012-2014 SCRIPT_AUTHOR
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy of
 // this software and associated documentation files (the "Software"), to deal in
