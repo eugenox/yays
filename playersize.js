cf7b19272a3fad5c81a63e3cbec7692e86a6a3ba
eugenox <eugenox@gmail.com>
2014-07-05 15:57:42 +0200

    Handle theater mode.

--- playersize.js
+++ playersize.js
@@ -5,10 +5,11 @@ function PlayerSize(player) {
 	PlayerOption.call(this, player, 'player_size');
 }
 
-#define CONTENT_WIDTH 1040
-#define PLAYER_WIDTH   854
-#define PLAYER_HEIGHT  480
-#define CONTROL_HEIGHT  30
+#define CONTENT_WIDTH    1040
+#define PLAYER_WIDTH      854
+#define PLAYER_HEIGHT     480
+#define CONTROL_HEIGHT     30
+#define THEATER_CORRECTION 10
 
 PlayerSize.prototype = extend(PlayerOption, {
 	apply: function() {
@@ -29,6 +30,9 @@ PlayerSize.prototype = extend(PlayerOption, {
 						'}',
 						'.watch-medium .player-height {',
 							CONCATENATE('height: ', EVALUATE(CONTENT_WIDTH / (PLAYER_WIDTH / PLAYER_HEIGHT) + CONTROL_HEIGHT), 'px !important;'),
+						'}',
+						'.watch-medium #theater-background {',
+							CONCATENATE('height: ', EVALUATE(CONTENT_WIDTH / (PLAYER_WIDTH / PLAYER_HEIGHT) + CONTROL_HEIGHT + THEATER_CORRECTION), 'px !important;'),
 						'}'
 					]
 				});
@@ -36,11 +40,12 @@ PlayerSize.prototype = extend(PlayerOption, {
 				// no break;
 
 			case 1: // WIDE
-				var container = DH.id('watch7-container'), player = DH.id('player');
+				var container = DH.id('watch7-container'), player = DH.id('player'), page = DH.id('page');
 
 				DH.addClass(container, 'watch-wide');
 				DH.delClass(player, 'watch-small');
 				DH.addClass(player, 'watch-medium');
+				DH.addClass(page, 'watch-stage-mode');
 
 				break;
 
